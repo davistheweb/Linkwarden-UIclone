137a0f65984bf62ca72bc5a2e588762d970e8bcc
davistheweb <smartkelvin775@gmail.com>
2024-12-15 16:01:58 +0100

    :

--- assets/js/users-comment.js
+++ assets/js/users-comment.js
@@ -1,5 +1,6 @@
 const userCommentTitlesElement = document.querySelectorAll('.article-comment-text'),
 userNamesElements = document.querySelectorAll('.usersname');
+const articleElements = document.querySelectorAll('.user-comment-box');
 const displayUserComments = () => {
     fetch('assets/js/user_comment.json')
     .then(res => res.json())
@@ -8,11 +9,14 @@ const displayUserComments = () => {
         const userNames = Object.values(UserCommentDetails.Usernames),
         userCommentTitles = Object.values(UserCommentDetails.UserComments);
         userCommentTitlesElement.forEach((ele, index) => {
-            ele.textContent =  `'"${userCommentTitles[index]}"'`
+            ele.textContent =  `'"${userCommentTitles[index]}"'`;
         });
         userNamesElements.forEach((ele, index) => {
-            ele.textContent =  userNames[index]
+            ele.textContent =  userNames[index];
         }); 
+        articleElements.forEach(art => {
+            art.addEventListener('click', () => window.open("https://x.com/davistheweb"));
+        });
     });
 };
 displayUserComments();
