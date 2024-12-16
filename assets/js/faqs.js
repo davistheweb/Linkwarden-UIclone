38ba8808f4410ca1b8988ced83ebe16f0572e7b7
davistheweb <smartkelvin775@gmail.com>
2024-12-16 01:24:28 +0100

    :

--- assets/js/faqs.js
+++ assets/js/faqs.js
@@ -1,9 +1,18 @@
+const FaqQuestionElement = document.querySelectorAll('.faq-text'),
+    FaqAnswerElement = document.querySelectorAll('.faq-answer_text')
 const fetchFaqs = async () => {
     const data = await fetch('assets/js/faqs.json', {'method' : "GET"}),
         response = await data.json(),
         responseData = response[0],
-        FaqsQuestion = Object.values(responseData.FaqS),
+        FaqsQuestions = Object.values(responseData.FaqS),
         faqsAnswer = Object.values(responseData.FaqAnswer);
+
+        FaqQuestionElement.forEach((ele, index) => {
+            ele.textContent = FaqsQuestions[index];
+        });
+        FaqAnswerElement.forEach((ele, index) => {
+            ele.innerHTML = `<p class="faq-answer_text">${faqsAnswer[index]}</p>`;
+        })
 
     
 }
