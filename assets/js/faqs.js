cfbd448ef1d364ee7643d70069655fc3ca1d5dba
davistheweb <smartkelvin775@gmail.com>
2024-12-22 16:25:02 +0100

    updated faq section click event

--- assets/js/faqs.js
+++ assets/js/faqs.js
@@ -1,7 +1,9 @@
 const FaqQuestionElement = document.querySelectorAll('.faq-text'),
     FaqAnswerElement = document.querySelectorAll('.faq-answer_text'),
     faqParentElement = document.querySelectorAll('.faq-question'),
-    faqAnswerParentElement = document.querySelectorAll('.faq-answer');
+    faqAnswerParentElement = document.querySelectorAll('.faq-answer'),
+    faqOpenElement = document.querySelectorAll('.faqp'),
+    faqCloseElement = document.querySelectorAll('.faqx');
 const fetchFaqs = async () => {
     const data = await fetch('assets/js/faqs.json', {'method' : "GET"}),
         response = await data.json(),
@@ -17,16 +19,29 @@ const fetchFaqs = async () => {
         });
         faqParentElement.forEach((elem, index) => {
             elem.addEventListener('click', () => {
-                const faqAnswerisAlreadyOpen = faqAnswerParentElement[index].classList.contains('popup');
+                faqOpenElement.forEach((ele, i) => {
+                    if (i !== index) ele.classList.remove('rotate-close-open');
+                });
+        
+                faqOpenElement[index].classList.toggle('rotate-close-open');
+        
+                faqCloseElement.forEach((ele, i) => {
+                    if (i !== index) ele.classList.remove('rotate-close--open');
+                });
+        
+                faqCloseElement[index].classList.toggle('rotate-close--open');
                 
+                const faqAnswerisAlreadyOpen = faqAnswerParentElement[index].classList.contains('popup');
+        
                 faqAnswerParentElement.forEach(faqAnswer => {
                     faqAnswer.classList.remove('popup');
                 });
-                
+        
                 if (!faqAnswerisAlreadyOpen) {
                     faqAnswerParentElement[index].classList.add('popup');
                 }
             });
-        });      
+        });
+        
 };
 fetchFaqs();
